051402ba945fd9c927b8baaec90a1e4857c92d36
Cleiton Ferreira <cleitonrferreira@gmail.com>
2023-02-16 14:33:35 -0300

    refspec

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -19,10 +19,11 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  
   isAuthenticated() : boolean {
     return false;
   }
-  
+
 
   salvar(usuario: Usuario) : Observable<any>{
     return this.http.post<any>(this.apiURL, usuario);
